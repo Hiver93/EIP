587ea9508b6d44a1a88a865ebeff9694bed4823f
kdw3238 <kdw3238@naver.com>
2024-04-21 22:57:49 +0900

    [fix] workbook 엔티티 생성자 추가

--- back-end/eip/src/main/java/com/kdw/eip/workbook/domain/entity/Workbook.java
+++ back-end/eip/src/main/java/com/kdw/eip/workbook/domain/entity/Workbook.java
@@ -12,13 +12,17 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "workbook")
 @Data
 @Builder
 @EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Workbook {
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "id", updatable = false)
